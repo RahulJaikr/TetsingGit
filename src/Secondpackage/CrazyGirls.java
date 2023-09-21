408b837866461aa6528824c41a612eea956870be
RahulJaikr <Rahul_Jain_@Outlook.com>
2023-09-22 00:24:44 +0530

    most wanted

--- src/Secondpackage/CrazyGirls.java
+++ src/Secondpackage/CrazyGirls.java
@@ -4,7 +4,18 @@ public class CrazyGirls {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ChromeDriver - ChromeDriver ->Methods close 
+		//Firefox- FirefoxDriver ->Methods close
+		//safari SaariDrier ->methods close get
+		//WebDriver close get
+	    //WebDriver methods + class methods 
+			
+		// chromedriver.exe-> Chrome browser
+		
+		System.out.println("WebAutomation");
+		System.out.println("Rohit Kohli");
+		
+		
 	}
 
 }
