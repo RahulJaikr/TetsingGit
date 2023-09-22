2c4c7fbbaea905e4bd1ba87b991b148a28be4771
RahulJaikr <Rahul_Jain_@Outlook.com>
2023-09-22 18:40:56 +0530

    Asian Team

--- src/Secondpackage/ChromeDriver.java
+++ src/Secondpackage/ChromeDriver.java
@@ -2,4 +2,14 @@ package Secondpackage;
 
 public class ChromeDriver {
 
+	public void sysout()
+	{
+		// TODO Auto-generated method stub
+		int i =0;
+		int a =7;
+	int 	c = i + a;
+		System.out.println(c);
+			
+
+	}
 }
